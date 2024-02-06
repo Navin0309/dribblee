c0704d475d7538582cbfe9f4ed659a88297e2582
Navin0309 <navinkumarilango0309@gmail.com>
2024-02-06 21:17:39 +0530

    n

--- src/components/Footer/ItemsContainer.jsx
+++ src/components/Footer/ItemsContainer.jsx
@@ -1,12 +1,11 @@
-import Item from "./Item";
-import { PRODUCTS, RESOURCES, COMPANY, SUPPORT } from "./Menus";
 const ItemsContainer = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16">
-      <Item Links={PRODUCTS} title="PRODUCTS" />
-      <Item Links={RESOURCES} title="RESOURCES" />
-      <Item Links={COMPANY} title="COMPANY" />
-      <Item Links={SUPPORT} title="SUPPORT" />
+    <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-13 px-6 py-16">
+
+      <div>For designers</div>
+      <div>Inspiration</div>
+      <div>Careers</div>
+      <div>Support</div>
     </div>
   );
 };
