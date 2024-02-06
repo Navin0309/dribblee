08a9c3a317ca6bdca5827f704c5fcfcb0171b619
Navin0309 <navinkumarilango0309@gmail.com>
2024-02-06 21:28:58 +0530

    n

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
           </button>
         </div>
       </div>
-      // <ItemsContainer/>
+      <ItemsContainer/>
       <div
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
       text-center pt-2 text-gray-400 text-sm pb-8"
